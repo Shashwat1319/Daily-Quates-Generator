a24a6fee9b059334665f7fafc151ef1d858ec22d
= <pymagic140@gmail.com>
2026-01-13 07:01:23 +0530

    Add basic navigation structure with Navbar

---
+++ client/src/Components/layout/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+   <nav className='flex justify-between items-center px-6 py-4'>
+        <h1 className='text-xl font-semibold'>Daily Quotes Generator</h1>
+        <ul className='flex gap-6 text-base'>
+            <li className='cursor-pointer hover:underline'>Home</li>
+            <li className='cursor-pointer hover:underline'>Quotes</li>
+            <li className='cursor-pointer hover:underline'>Contact Us</li>
+            <li className='cursor-pointer hover:underline'>Sign up</li>
+        </ul>
+   </nav>
+    </>
+  )
+}
+
+export default Navbar
